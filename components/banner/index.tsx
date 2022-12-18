b11a52bf39fbbff2b4bc356dab56b751f9567f18
Ishan Baniya <ibaniya98@gmail.com>
2022-12-18 15:32:46 -0800

    add image for jumbrotron

--- components/banner/index.tsx
+++ components/banner/index.tsx
@@ -1,4 +1,5 @@
-import { Box, Button, Divider, Heading, Text } from "@chakra-ui/react";
+import { Box, Button, Divider, Grid, Heading, Text } from "@chakra-ui/react";
+import Image from "next/image";
 import Link from "next/link";
 
 function OpenTime() {
@@ -7,17 +8,36 @@ function OpenTime() {
 
 export default function Banner() {
   return (
-    <Box p="5" textAlign="center">
-      <Heading size="xl">Liana Eyebrow Threading and Spa</Heading>
-      <Divider my="3" />
-      <Text>Eyebrow Bar in Savannah</Text>
-      <OpenTime />
+    <Box p="5" m="5">
+      <Grid gridTemplateColumns={{ base: "1fr", md: "3fr 2fr" }} gap="5">
+        <Box display="flex" justifyContent="center" flexDir="column">
+          <Heading size="xl">Liana Eyebrow Threading and Spa</Heading>
 
-      <Button variant="purple" my="5" p="6">
-        <Link href="https://www.fresha.com/a/liana-eyebrow-threading-spa-savannah-15-east-montgomery-cross-road-xdqg1yep/">
-          Book Now
-        </Link>
-      </Button>
+          <Divider my="5" maxW="600px" borderColor="gray.300" />
+
+          <Box fontSize="lg">
+            <Text>Eyebrow Bar in Savannah</Text>
+            <OpenTime />
+          </Box>
+
+          <Button variant="purple" my="5" p="6" maxW="300px">
+            <Link href="https://www.fresha.com/a/liana-eyebrow-threading-spa-savannah-15-east-montgomery-cross-road-xdqg1yep/">
+              Book Now
+            </Link>
+          </Button>
+        </Box>
+        <Box w="100%" position="relative" display="inline-block" minH="300px">
+          <Image
+            src="/images/eyebrow-one.jpg"
+            alt="Eyebrow threading"
+            fill
+            style={{
+              objectFit: "cover",
+              borderRadius: "var(--chakra-radii-md)",
+            }}
+          />
+        </Box>
+      </Grid>
     </Box>
   );
 }
