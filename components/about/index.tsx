6730d012fbbdc912cc0e8a9073ca0be40b312a21
Ishan Baniya <ibaniya98@gmail.com>
2022-12-23 21:50:05 -0800

    update box shadow

--- components/about/index.tsx
+++ components/about/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, ChakraProps, Heading, SimpleGrid, Text } from "@chakra-ui/react";
 import { ReactNode, createElement } from "react";
 import { GiComb, GiPeaceDove } from "react-icons/gi";
 import { VscSmiley } from "react-icons/vsc";
@@ -8,16 +8,17 @@ interface TileProps {
   title: string;
   icon: ReactNode;
   bgGradient: string;
+  boxShadow?: string;
 }
 
-function Tile({ title, bgGradient, icon }: TileProps) {
+function Tile({ title, bgGradient, icon, boxShadow }: TileProps) {
   return (
     <Box
       w="100%"
       maxW="200px"
       p="10"
       borderRadius="lg"
-      boxShadow="lg"
+      boxShadow={boxShadow || "md"}
       minH="100px"
       textAlign="center"
       textColor="white"
@@ -28,9 +29,9 @@ function Tile({ title, bgGradient, icon }: TileProps) {
         {icon}
       </Box>
 
-      <Heading size="sm" letterSpacing="wide">
+      <Text fontSize="md" letterSpacing="wide">
         {title}
-      </Heading>
+      </Text>
     </Box>
   );
 }
@@ -38,26 +39,30 @@ function Tile({ title, bgGradient, icon }: TileProps) {
 export default function About() {
   return (
     <Box>
-      <SimpleGrid columns={{ base: 1, sm: 2 }} gap="5">
+      <SimpleGrid columns={{ base: 1, sm: 2 }} gap="10">
         <Tile
           title="Honest Pricing"
           bgGradient="linear(40deg, #e87e16 0%, #f0de14 100%)"
+          boxShadow="0px 20px 40px 0px rgb(224 149 32 / 40%)"
           icon={<BsCurrencyDollar />}
         />
         <Tile
           title="Expert Stylists"
           bgGradient="linear(40deg, #5e2ced 0%, #a485fd 100%)"
+          boxShadow="0px 20px 40px 0px rgb(94 44 237 / 40%)"
           icon={<GiComb />}
         />
 
         <Tile
           title="Comfortable Atmosphere"
           bgGradient="linear(40deg, #2171d4 0%, #2cc4f0 100%)"
+          boxShadow="0px 20px 40px 0px rgb(44 130 237 / 40%)"
           icon={<GiPeaceDove />}
         />
         <Tile
           title="Satisfied Customers"
           bgGradient="linear(40deg, #e03827 0%, #f9a47a 100%)"
+          boxShadow="0px 20px 40px 0px rgb(224 56 39 / 40%)"
           icon={<VscSmiley />}
         />
       </SimpleGrid>
